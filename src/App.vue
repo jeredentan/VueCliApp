45fb1a06bb30cb58f60cd16de2188195dc676ac5
jeremie dentan <jeremie.dentan@sword-group.com>
2018-06-15 00:01:13 +0200

    Ajout d'une navigation (WIP) + ajout d'une authent + css precompilers

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
-q<template>
+<template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <!-- <app-nav></app-nav> -->
     <router-view/>
   </div>
 </template>
+
+
+
+<!-- <script>
+import { Component, Vue } from 'vue-property-decorator';
+import AppNav from '@/components/AppNav.vue';
+
+@Component({
+  components: {
+    AppNav,
+  },
+})
+export default class App extends Vue {
+  constructor() {
+    super();
+    console.log(process.env.VUE_APP_API);
+  }
+}
+</script> -->
 
 <style lang="less">
 #app {
